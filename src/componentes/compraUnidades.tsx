6b21d38727ab05efa46c7c7129d75d008ac33b92
Benjamin Navarro <benjaminns_l@outlook.es>
2025-11-27 23:48:49 -0600

    estiliza el componente de menu compra unidades

--- src/componentes/compraUnidades.tsx
+++ src/componentes/compraUnidades.tsx
@@ -12,15 +12,18 @@ export type unidadCompra = {
 
 export function CompraUnidadesMenu ({ listaUnidades }:{listaUnidades:unidadCompra[]}){
   return (
-    <div className='menu-compra-unidades'>
-      {listaUnidades.map(UnidadCompra => {
-        return UnidadCompra.habilitado && (
-          <div className='opcion-compra' onClick={UnidadCompra.clickHandler} key={UnidadCompra.nombre}>
-            {/* <img src={UnidadCompra.spriteUrl} alt="" className='icon'/> */}
-            <div className='nombre'>{UnidadCompra.nombre}</div>
-            <div className='costo'>{UnidadCompra.costo}</div>
-          </div>
-        )})}
+    <div className='absolute left-0 top-0' style={{ background: '#33333355', width: '100%', height: '100%' }}>
+      <div className='menu-compra-unidades grid grid-cols-2 grid-rows-8 bg-amber-50 rounded-sm absolute p-2 gap-1 shadow-2xl shadow-gray-500 scroll-auto'>
+        <h1 className='col-span-2 text-center font-bold'>Compra unidades</h1>
+        {listaUnidades.map(UnidadCompra => {
+          return UnidadCompra.habilitado && (
+            <div className='opcion-compra bg-amber-200 hover:bg-amber-400 flex p-2 gap-.5 cursor-pointer rounded-sm' onClick={UnidadCompra.clickHandler} key={UnidadCompra.nombre}>
+              <img src='./img/unidades/explosion.gif' alt={UnidadCompra.nombre} className='icon'/>
+              <div className='nombre flex flex-col justify-center'>{UnidadCompra.nombre}</div>
+              <div className='costo flex flex-col justify-center font-bold text-right'>{UnidadCompra.costo}</div>
+            </div>
+          )})}
+      </div>
     </div>
   )
 }
