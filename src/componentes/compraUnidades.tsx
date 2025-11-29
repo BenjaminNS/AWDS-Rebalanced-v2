e3e43f6cc1377852eb5125c4d0afcb3e62aeff54
Benjamin Navarro <benjaminns_l@outlook.es>
2025-11-28 21:01:01 -0600

    Cambia nombre de la segunda variable, no tiene valor predefinido y solo oculta o muestra el menú de compra, no lo elimina

--- src/componentes/compraUnidades.tsx
+++ src/componentes/compraUnidades.tsx
@@ -9,9 +9,9 @@ export type unidadCompra = {
   clickHandler: ()=>void
 }
 
-export function CompraUnidadesMenu ({ listaUnidades, menuHabilitado = true }:{listaUnidades:unidadCompra[], menuHabilitado: boolean}){
-  return menuHabilitado && (
-    <div className={'absolute left-0 top-0' + menuHabilitado ? 'hidden' : '' } style={{ background: '#33333355', width: '100%', height: '100%' }}>
+export function CompraUnidadesMenu ({ listaUnidades, propiedadSeleccionada }:{listaUnidades:unidadCompra[], propiedadSeleccionada: boolean}){
+  return (
+    <div className={propiedadSeleccionada ? 'absolute left-0 top-0' : 'hidden' } style={{ background: '#33333355', width: '100%', height: '100%' }}>
       <div className='menu-compra-unidades grid grid-cols-2 grid-rows-8 bg-amber-50 rounded-sm absolute p-2 gap-1 shadow-2xl shadow-gray-500 scroll-auto'>
         <h1 className='col-span-2 text-center font-bold'>Compra unidades</h1>
         {listaUnidades.map(UnidadCompra => {
