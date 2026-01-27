640df1c42cb4c918be6516d738a8903644ed9032
Benjamin Navarro <benjaminns_l@outlook.es>
2026-01-27 16:41:43 -0600

    feat-refactor: privatiza las propiedades y agrega habilidad de dia a dia, poderes y carga maxima a Comandante

--- src/comandantes/comandante.ts
+++ src/comandantes/comandante.ts
@@ -199,29 +199,61 @@ export class ComandanteJugable{
 }
 
 export class Comandante{
-  nombre: string
-  descripcion: string
-  pais: nombresPaises
-  cancion: AudioData|null
-  // spritesComandante: spritesComandante; //Talvez sean coordenadas como los terrenos
+  #nombre: string
+  #nombreCorto: string
+  #descripcion: string
+  #pais: nombresPaises
+  #cancion: AudioData|null
   // canciones: {normal: AudioData, cop: AudioData, scop: AudioData};
+  // spritesComandante: spritesComandante; //Talvez sean coordenadas como los terrenos
 
-  // d2d: DayToDay;
+  #d2d: DayToDay
   // Definiría como se cargan y como se activan los poderes de los comandantes
   // Advance Wars 2: carga aumenta basado en el valor de las unidades por el daño hecho (60%) y recibido (90%)
   // y puede ser activado sin unidad con zona de comandante
   // Days of Ruin: Los poderes cargan solamente por daño hecho a unidades enemigas
   // Si pierdes tu unidad con zona de comandante, pierdes toda tu carga
   // Debes gastar el turno de tu unidad con CO para activar el poder (no puede atacar el mismo turno)
-  // tipoCarga: 'Advance Wars 2'|'Days of Ruin';
-  poderes: ComandantePoder[]|null
+  // tipoCarga: 'Advance Wars 2'|'Days of Ruin'
+
+  // #estrellas: number
+  #limiteCarga: number|null
+  #poderes: ComandantePoder[]|null
 
   // Funciones de daño, ataque, defensa, costo, movimiento, rango, etc.
-  constructor (nombre: string, descripcion: string, pais: nombresPaises, cancion: AudioData|null){
-    this.nombre = nombre
-    this.descripcion = descripcion
-    this.pais = pais
-    this.poderes = null
-    this.cancion = cancion
+  constructor (nombre: string, nombreCorto: string, descripcion: string, pais: nombresPaises, d2d: DayToDay, limiteCarga: number|null, poderes: ComandantePoder[]|null, cancion: AudioData|null){
+    this.#nombre = nombre
+    this.#nombreCorto = nombreCorto
+    this.#descripcion = descripcion
+    this.#pais = pais
+    this.#d2d = d2d
+    this.#limiteCarga = limiteCarga
+    this.#poderes = poderes
+    this.#cancion = cancion
+  }
+
+  getNombre (){
+    return this.#nombre
+  }
+  getNombreCorto (){
+    return this.#nombreCorto
+  }
+  getDescripcion (){
+    return this.#descripcion
+  }
+  getPais (){
+    return this.#pais
+  }
+  getCancion (){
+    return this.#cancion
+  }
+  getDayToDay (){
+    return this.#d2d
+  }
+  getLimiteCarga (){
+    return this.#limiteCarga
+  }
+  getPowers (){
+    return this.#poderes
   }
 }
