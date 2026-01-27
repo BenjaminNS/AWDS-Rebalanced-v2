81cef0589ab2f5d057ec75f8ae7f81fb2b2f2ca3
Benjamin Navarro <benjaminns_l@outlook.es>
2026-01-27 17:11:35 -0600

    fix-feat: corrige la generacion de comandante jugable y retorna 3 usuarios (jugadores) ahora

--- src/mocks/jugadoresMocks.ts
+++ src/mocks/jugadoresMocks.ts
@@ -1,5 +1,10 @@
 import { Jugador } from '../jugador'
 import { ComandanteJugable } from '../comandantes/comandante'
 
-export const usuario1 = new Jugador('Juan Sabor', 'A', true, null, [new ComandanteJugable('Andy', crypto.randomUUID(), 50000, 0, 0, true)])
-export const usuario2 = new Jugador('Fatboy Slim', 'B', true, null, [new ComandanteJugable('Andy', crypto.randomUUID(), 50000, 0, 0, true)])
+const Andy = await ComandanteJugable.crear('Andy', crypto.randomUUID(), 50000, 0, 0, true, 'normal')
+const Generico = await ComandanteJugable.crear('Generico', crypto.randomUUID(), 50000, 0, 0, true, 'normal')
+const Max = await ComandanteJugable.crear('Max', crypto.randomUUID(), 30000, 0, 0, true, 'normal')
+
+export const usuario1 = new Jugador('Juan Sabor', 'A', true, null, [Andy], '#b64f54')
+export const usuario2 = new Jugador('NPC1234', 'B', true, null, [Generico], '#4f85b6')
+export const usuario3 = new Jugador('Startank', 'B', true, null, [Max], '#66A068')
