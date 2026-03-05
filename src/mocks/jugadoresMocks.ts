f0cb6d1c7e379c475c7ad3f8fe449a45b1f8aee6
Benjamin Navarro <benjaminns_l@outlook.es>
2026-03-04 20:32:08 -0600

    fix: genera instancias de comandantes distintas para cada jugador en el mock de jugadores

--- src/mocks/jugadoresMocks.ts
+++ src/mocks/jugadoresMocks.ts
@@ -11,8 +11,11 @@ import { Sami_Comandante } from '../comandantes/Sami_Comandante'
 
 // const Andy = new ComandanteJugable('Andy', 50000, 0, 0, true, 'normal', ComandanteAndy)
 const ComandanteSami1 = new Sami_Comandante({ activo: true, cargaActual: 21000, dineroActual: 20000, statusEffects: [], usosPoder: 0 }, { ref: null, id: 'string' })
+const ComandanteSami2 = new Sami_Comandante({ activo: true, cargaActual: 15000, dineroActual: 10000, statusEffects: [], usosPoder: 0 }, { ref: null, id: 'string' })
+const ComandanteSami3 = new Sami_Comandante({ activo: true, cargaActual: 30000, dineroActual: 30000, statusEffects: [], usosPoder: 0 }, { ref: null, id: 'string' })
 // const Max = new ComandanteJugable('Max', 30000, 0, 0, true, 'normal', ComandanteGenerico)
 
 export const usuario1 = new Jugador('Juan Sabor', 'A', true, null, [ComandanteSami1], '#b64f54')
-export const usuario2 = new Jugador('NPC1234', 'B', true, null, [ComandanteSami1], '#4f85b6')
-export const usuario3 = new Jugador('Startank', 'B', true, null, [ComandanteSami1], '#66A068')
+export const usuario2 = new Jugador('NPC1234', 'B', true, null, [ComandanteSami2], '#4f85b6')
+export const usuario3 = new Jugador('Startank', 'B', true, null, [ComandanteSami3], '#66A068')
+
