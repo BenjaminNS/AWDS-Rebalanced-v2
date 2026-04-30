e346c28183bd75927dd84f2ee7c41aa8e354bb02
Benjamin Navarro <benjaminns_l@outlook.es>
2026-04-30 13:44:57 -0600

    feat: agrega modelo de unidad recon

---
+++ src/unidades/modelos/recon.ts
@@ -0,0 +1,33 @@
+import { UnidadCasilla, type datosActuales } from '../unidadCasilla'
+import { spriteRecon } from '../spriteUnidades'
+import { recon_MU } from '../matchups'
+import { type ComandanteBase } from '../../comandantes/ComandanteBase'
+import type { Casilla } from '../../mapa/casilla'
+import { tamanoSprite, UnitSpritesheet } from '../unidadesSpriteConf'
+import Konva from 'konva'
+
+export class Infanteria extends UnidadCasilla {
+
+  constructor (refComandante: ComandanteBase, casilla: Casilla, datosActuales: datosActuales){
+    super({ nombreLargo: 'Recon', nombreCorto: 'recon', descripcion: 'Vehículo pequeño con alta visión, eficiente contra soldados.', categorias: ['Vehiculo', 'Terrestre', 'Directo'],
+      costo: 4000, rango: { minimo: 1, extra: 0 }, movilidad: 8, tipoMovimiento: 'ruedas', vision: 2, maxGasolina: 40, consumoDiario: () => 0, maxMuniciones: { 'principal': { actual: 8, maxima: 8 } }, contraataque: 1, atacarYMoverse: true, matchups: recon_MU, sprite: spriteRecon
+    }, datosActuales, casilla )
+
+  }
+
+  override getConsumoDiario (): number {
+    return 0
+  }
+
+  override generarSpriteInfo (): Konva.Sprite {
+    return new Konva.Sprite({
+      image: UnitSpritesheet,
+      animations: {
+        'idle': [
+          (3 * tamanoSprite), (0 * tamanoSprite), tamanoSprite, tamanoSprite
+        ]
+      },
+      animation: 'idle', frameRate: 3, frameIndex: 0
+    })
+  }
+}
