892740e5771307587688c17dba4991b402a85524
Benjamin Navarro <benjaminns_l@outlook.es>
2026-05-25 21:35:39 -0600

    fix: hace opcional el dato de comandante en constructor

--- src/unidades/modelos/infanteria.ts
+++ src/unidades/modelos/infanteria.ts
@@ -1,18 +1,16 @@
 import { UnidadCasilla, type datosActuales } from '../unidadCasilla'
 import { spriteInfanteria } from '../spriteUnidades'
 import { infanteria_MU } from '../matchups'
-import { type ComandanteBase } from '../../comandantes/ComandanteBase'
-import type { Casilla } from '../../mapa/casilla'
 import { tamanoSprite, UnitSpritesheet } from '../unidadesSpriteConf'
 import Konva from 'konva'
 
 export class Infanteria extends UnidadCasilla {
   #puntosCapturaActual: number|null|undefined
 
-  constructor (refComandante: ComandanteBase, casilla: Casilla, datosActuales: datosActuales, params?: { capturaActual?: number|null }){
-    super({ nombreLargo: 'Infantería', nombreCorto: 'infanteria', descripcion: 'Soldado capaz de capturar propiedades.', categorias: ['Soldado', 'Terrestre', 'Directo'],
-      costo: 1000, rango: { minimo: 1, extra: 0 }, movilidad: 3, tipoMovimiento: 'pie', vision: 2, maxGasolina: 40, maxMuniciones: { 'principal': { actual: 6, maxima: 6 } }, contraataque: 1, atacarYMoverse: true, matchups: infanteria_MU, sprite: spriteInfanteria
-    }, datosActuales, casilla )
+  constructor (datosActuales: datosActuales, params?: { capturaActual?: number|null }){
+    super({ nombreLargo: 'Infantería', nombreCorto: 'infanteria', descripcion: 'Soldado capaz de capturar propiedades.', categorias: ['Terrestre', 'Directo'],
+      costoOro: 1000, rango: { minimo: 1, extra: 0 }, movilidad: 3, tipoMovimiento: 'pie', vision: 2, maxGasolina: 40, maxMuniciones: { 'principal': { actual: 6, maxima: 6 } }, contraataque: 1, atacarYMoverse: true, matchups: infanteria_MU, sprite: spriteInfanteria
+    }, datosActuales )
 
     this.#puntosCapturaActual = params?.capturaActual
   }
