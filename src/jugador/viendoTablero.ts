00dca4b87faee97354c1c07e8b0b05bb35934bcd
Benjamin Navarro <benjaminns_l@outlook.es>
2026-05-28 23:45:33 -0600

    feat: actualiza info casilla en evento de hover

--- src/jugador/viendoTablero.ts
+++ src/jugador/viendoTablero.ts
@@ -38,9 +38,42 @@ const rightClickHandler = (mousePosition: {x: number, y: number}, params: params
   // }
 }
 const rightClickReleaseHandler = (mousePosition: {x: number, y: number}, params: paramsJugadorState) => {}
-const hoverMouseHandler = (mousePosition: {x: number, y: number}, params: paramsJugadorState) => {}
+const hoverMouseHandler = (mousePosition: {x: number, y: number}, params: paramsJugadorState) => {
+  const pos = params.konvaMapa.getKonvaStage()?.getPointerPosition()
+  console.log(`hoverMouseHandler viendo tablero: ${pos}`)
+  if (!pos) return
 
+  // return
 
+  if ( !canHoverFlag )
+    return
+
+  // if se tiene abierto un menú (los botones tienen el evento de mouseover) return
+  // const pos = params.konvaMapa.getKonvaStage()?.getPointerPosition()
+  // if (!pos) return
+  // debugger
+  
+  // TO-DO: obtener la casilla (no funciona porque mapaJuego no tiene definido konvaStage)
+  const casillaHover = params.getCasilla(pos, params.tamanoCasilla)
+  // const coordHover = { x: Math.floor(pos.x / params.konvaMapa.getTamanoCasilla()), y: Math.floor(pos.y / params.konvaMapa.getTamanoCasilla()) }
+  // const casillaHover = params.getCasilla(pos, params.tamanoCasilla) as Casilla
+  if ( casillaHover == null ) return
+
+  // params.cursorImg.x(coordHover.x * params.konvaMapa.getTamanoCasilla())
+  // params.cursorImg.y(coordHover.y * params.konvaMapa.getTamanoCasilla())
+
+  params.setInfoCasilla({
+    estrellas: casillaHover.getEstrellasDefensa(),
+    gasActual: casillaHover.getUnidad()?.getGasActual(),
+    gasMaxima: casillaHover.getUnidad()?.getMaxGasolina(),
+    hp: casillaHover.getUnidad()?.getHp(),
+    municionesPrincipales: casillaHover.getUnidad()?.getMunicionPrincipal(),
+    municionesSecundarias: casillaHover.getUnidad()?.getMunicionSecundaria(),
+    status: casillaHover.getUnidad()?.getEstado(),
+    terreno: casillaHover.getNombreCorto()
+  })
+  // params.actualizarInfoCasilla(coordHover, casillaHover)
+}
 
 export class viendoTableroState extends jugadorState{
   constructor (params: paramsJugadorState){
